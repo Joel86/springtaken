37c745e19db8545822c775e0df5c51cf157c109b
Joel Vanherle <joel.vanherle@hotmail.com>
2017-11-13 11:44:40 +0100

    List injection persooneigenschappen

--- src/main/java/be/vdab/presentation/PersoonViewer.java
+++ src/main/java/be/vdab/presentation/PersoonViewer.java
@@ -3,23 +3,36 @@ package be.vdab.presentation;
 import java.util.List;
 
 import be.vdab.entities.Persoon;
+import be.vdab.enums.PersoonEigenschap;
 
 public class PersoonViewer {
-	private boolean aantalKinderenTonen;
-	PersoonViewer(boolean aantalKinderenTonen) {
-		this.aantalKinderenTonen = aantalKinderenTonen;
-	}
-	public void setAantalKinderenTonen(boolean aantalKinderenTonen) {
-		this.aantalKinderenTonen = aantalKinderenTonen;
+	private final PersoonEigenschap[] eigenschappen;
+	public PersoonViewer(PersoonEigenschap[] eigenschappen) {
+		this.eigenschappen = eigenschappen;
 	}
 	public void afbeelden(List<Persoon> personen) {
 		for (Persoon persoon : personen) {
-			System.out.printf("%d %s %s ", persoon.getPersoonNr(),
-			persoon.getVoornaam(), persoon.getFamilienaam());
-			if (aantalKinderenTonen) {
-				System.out.print(persoon.getAantalKinderen());
+			for(PersoonEigenschap eigenschap : eigenschappen) {
+				toonEigenschap(persoon, eigenschap);
+				System.out.print(' ');
 			}
 			System.out.println();
+		}
+	}
+	private void toonEigenschap(Persoon persoon, PersoonEigenschap eigenschap) {
+		switch(eigenschap) {
+		case PERSOON_NR:
+			System.out.print(persoon.getPersoonNr());
+			break;
+		case VOORNAAM:
+			System.out.print(persoon.getVoornaam());
+			break;
+		case FAMILIENAAM:
+			System.out.print(persoon.getFamilienaam());
+			break;
+		case AANTAL_KINDEREN:
+			System.out.print(persoon.getAantalKinderen());
+			break;
 		}
 	}
 }
